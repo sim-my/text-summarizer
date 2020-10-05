57873115913b53bd9e86ed09aabe986bd0cc5ed4
sim-my <simranpanthi101@gmail.com>
2020-10-05 17:43:42 +0545

    Completed functionality of the project

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
+from model import main
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def submit():
     if request.method == 'POST':
-        input_text = request.form.get("input_text")
-        print(input_text)
-        return render_template('app.html')
+        input_text = request.form.get("input_text")       
+        final_summary=main(input_text)
+        return render_template('app.html',summary=final_summary, input_text=input_text)
     else:
         return render_template('app.html')
+
+
+
